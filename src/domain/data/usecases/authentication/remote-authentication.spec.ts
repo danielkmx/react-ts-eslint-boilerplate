dffddccff28bce4cbda5e4c95e059c4c589f3966
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-27 14:44:10 -0300

    chore: mudando class de spy pra folder de data/test

--- src/domain/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,15 +1,8 @@
 import { RemoteAuthentication } from './remote-authentication'
-import { HttpPostClient } from '../../protocols/http/http-post.client'
+import { HttpPostClientSpy } from '../../test/mock-http-client'
 
 describe('Remote Auth' ,() => {
   test('Should call HttpClient with correct URL', async () => {
-    class HttpPostClientSpy implements HttpPostClient {
-      url?: string
-      async post (url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
     const url = 'any_url'
     const httpClient = new HttpPostClientSpy()
     const sut = new RemoteAuthentication(httpClient,url)
