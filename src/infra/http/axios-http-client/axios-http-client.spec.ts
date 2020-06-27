b2a751a63638406266f2d69356a55e609d9f9eb5
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-27 20:46:39 -0300

    chore: instalando react e ajeitando erro delint

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -22,12 +22,12 @@ const makeSut = (): SutTypes => {
 describe('AxiosHttpClient', () => {
   test('SHould call axios with correct values', async () => {
     const request = mockPostRequest()
-    const { sut,mockedAxios } = makeSut()
+    const { sut, mockedAxios } = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
-  test('Should return the correct statusCode and body', async () => {
-    const { sut,mockedAxios } = makeSut()
+  test('Should return the correct statusCode and body', () => {
+    const { sut, mockedAxios } = makeSut()
     const promise = sut.post(mockPostRequest())
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value)
   })
