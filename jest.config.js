f6448234ccce20a76245d3bea71340f6fc515671
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-27 10:49:25 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,22 @@
 module.exports = {
-  roots: ["<rootDir>/src"],
-  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}"],
-  coverageDirectory: "coverage",
-  testEnvironment: "node",
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/**/index.ts',
+    '!**/*.d.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/src/main/test/cypress'
+  ],
+  testEnvironment: 'jsdom',
   transform: {
-    ".+\\.ts$": "ts-jest",
+    '.+\\.(ts|tsx)$': 'ts-jest'
   },
-};
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.scss$': 'identity-obj-proxy'
+  }
+}
