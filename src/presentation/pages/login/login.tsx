803e31e2f72cc40ded65912114339fd41d683f12
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-28 19:49:02 -0300

    feat: mostrando mensagem de erro caso email seja invalido

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -18,7 +18,10 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     passwordError: 'Campo obrigatório'
   })
   useEffect(() => {
-    validation.validate('email',state.email)
+    setState({
+      ...state,
+      emailError: validation.validate('email',state.email)
+    })
   },[state.email])
   useEffect(() => {
     validation.validate('password',state.password)
