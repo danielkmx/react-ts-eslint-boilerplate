09b083847b437235bf253d671240c689dd82b4c0
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-28 15:45:41 -0300

    test: garantindo mensagem de erro na inicializacao

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,5 +9,11 @@ describe('Login component' , () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('Campo obrigatório')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('Campo obrigatório')
   })
 })
