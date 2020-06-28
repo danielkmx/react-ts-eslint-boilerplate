2a8f4e85095944a575ef1f6620c4382845ae099c
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-28 13:41:45 -0300

    refactor: isolando componente de input

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -3,20 +3,15 @@ import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
 import Footer from '@/presentation/components/footer/footer'
 import LoginHeader from '@/presentation/components/login-header/login.header'
+import Input from '@/presentation/components/input/input'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
       <LoginHeader />
       <form className={Styles.form}>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Digite seu e-mail" />
-          <span>Preenchimento obrigatório</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" placeholder="Digite sua senha" />
-          <span>Preenchimento obrigatório</span>
-        </div>
+        <Input type="email" name="email" placeholder="Digite seu email" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
         <div className={Styles.errorWrap}>
