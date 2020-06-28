93495b03ed6841982acf34d5aa5a17514e937601
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-28 15:25:43 -0300

    feat: garantindo q o submit button esta desabilitado

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -21,7 +21,7 @@ const Login: React.FC = () => {
         <form className={Styles.form}>
           <Input type="email" name="email" placeholder="Digite seu email" />
           <Input type="password" name="password" placeholder="Digite sua senha" />
-          <button type="submit">Entrar</button>
+          <button data-testid="submit" className={Styles.submit} disabled type="submit">Entrar</button>
           <span className={Styles.link}>Criar conta</span>
           <FormStatus />
         </form>
