ba666f15b9ee00bf9fe603fc4a869ff0868ddbd8
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-27 21:54:18 -0300

    chore: mudando versoes do lint

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    fileName: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', 'tsx', '.js'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  devServer: {
+    contentBase: './public',
+    writeToDisk: true,
+    historyApiFallback: true
+  },
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  }
+}
