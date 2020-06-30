a678e87c328d42866629a4befddef0c98e0204a6
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-30 14:43:44 -0300

    test: testando caso de sucesso do composite de erro

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -13,4 +13,14 @@ describe('ValidationComposite', () => {
     const error = sut.validate('any_field','any_value')
     expect(error).toBe('any_error_message')
   })
+  test('Should return error if any validation fails', () => {
+    const validationSpy = new FieldValidationSpy('any_field')
+    const validationSpy2 = new FieldValidationSpy('any_field')
+    const sut = new ValidationComposite([
+      validationSpy,
+      validationSpy2
+    ])
+    const error = sut.validate('any_field','any_value')
+    expect(error).toBeFalsy()
+  })
 })
