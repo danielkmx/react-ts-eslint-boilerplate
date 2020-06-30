64c3046f3ba96b24a62f16a574560ead81e49d1b
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-30 14:03:55 -0300

    refactor: criando factory no email validatio

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,14 +2,16 @@ import { InvalidEmailError } from '@/validation/errors'
 import EmailValidation from './email-validation'
 import faker from 'faker'
 
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation(faker.random.words())
+    const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new InvalidEmailError())
   })
   test('Should not return error if email is valid', () => {
-    const sut = new EmailValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
