32ed5fa07b9bec19d5775f24d83ea16c970e8b6c
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-30 14:00:59 -0300

    test: retornando se email é valido

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,16 @@
 import { InvalidEmailError } from '@/validation/errors'
 import EmailValidation from './email-validation'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
+    const sut = new EmailValidation(faker.random.words())
     const error = sut.validate('')
     expect(error).toEqual(new InvalidEmailError())
+  })
+  test('Should not return error if email is valid', () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
