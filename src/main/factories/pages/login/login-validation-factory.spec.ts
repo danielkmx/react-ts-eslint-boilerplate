f15e30ee5b5d4e856277f6aba6b4accbc3c18d11
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-30 21:49:25 -0300

    test: testando validacoes do login

---
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -0,0 +1,14 @@
+import { makeLoginValidation } from './login-validation-factory'
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+
+describe('loginValidationFactory',() => {
+  test('Deve criar ValidationComposite com valores corretos', () => {
+    const composite = makeLoginValidation()
+    console.log(composite)
+    expect(composite).toEqual(ValidationComposite.build([
+      ...ValidationBuilder.field('email').required().email().build(),
+      ...ValidationBuilder.field('password').required().minLength(5).build()
+    ]))
+  })
+})
