0c002fffe97e95ffc474b8763aac929d95c52818
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-06-30 21:15:47 -0300

    feat: adicionando composicao de login

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,22 +1,22 @@
 import { ValidationComposite } from './validation-composite'
-import { FieldValidationSpy } from '../test/mock-field-validation'
+import { FieldValidationSpy } from '@/validation/test/mock-field-validation'
 
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const validationSpy = new FieldValidationSpy('any_field')
     const validationSpy2 = new FieldValidationSpy('any_field')
     validationSpy2.error = new Error('any_error_message')
-    const sut = new ValidationComposite([
+    const sut = ValidationComposite.build([
       validationSpy,
       validationSpy2
     ])
     const error = sut.validate('any_field','any_value')
     expect(error).toBe('any_error_message')
   })
-  test('Should return error if any validation fails', () => {
+  test('Should not return any error', () => {
     const validationSpy = new FieldValidationSpy('any_field')
     const validationSpy2 = new FieldValidationSpy('any_field')
-    const sut = new ValidationComposite([
+    const sut = ValidationComposite.build([
       validationSpy,
       validationSpy2
     ])
