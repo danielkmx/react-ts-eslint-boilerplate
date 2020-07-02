c9b098a631f08eba0a992a2882780c4198a207c7
Daniel Bezerra <daniel.bezerra@somospi.com.br>
2020-07-01 21:33:46 -0300

    test: criando o metodo get na abstracao do localstorage

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -7,11 +7,20 @@ describe('LocalStorageAdapter', () => {
   beforeEach(() => {
     localStorage.clear()
   })
-  test('Deve chamar localStorage com valores corretos', () => {
+  test('Deve chamar localStorage.setItem com valores corretos', () => {
     const sut = new LocalStorageAdapter()
     const key = faker.database.column()
     const value = faker.random.objectElement<AccountDto>()
     sut.set(key,value)
     expect(localStorage.setItem).toHaveBeenCalledWith(key,JSON.stringify(value))
+  })
+  test('Deve chamar localStorage.getItem com valores corretos', () => {
+    const sut = new LocalStorageAdapter()
+    const key = faker.database.column()
+    const value = faker.random.objectElement<AccountDto>()
+    const getItemSpy = jest.spyOn(localStorage,'getItem').mockReturnValueOnce(JSON.stringify(value))
+    const obj = sut.get(key)
+    expect(obj).toEqual(value)
+    expect(getItemSpy).toHaveBeenCalledWith(key)
   })
 })
